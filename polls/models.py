099e954484b5dc0b5590224d67db98a29ca8eba2
akiyoko blog <akiyoko-team@users.noreply.github.com>
2014-11-30 20:53:36 +0900

    Fix max length

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Poll(models.Model):
-    question = models.CharField(max_length=200)
+    question = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
 
     def __unicode__(self):
